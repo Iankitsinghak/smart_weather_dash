a12a6692962ea768c441ecd3de051518ff401c1a
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-16 01:47:11 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures proper asset loading on Vercel and during local `build`
+  base: '/', // Ensures proper asset loading on Vercel and during local `build`
   build: {
     outDir: 'dist',
   },
