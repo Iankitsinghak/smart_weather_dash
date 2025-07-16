910adae0fb1ba06d9a0844e49e1587dfb038b186
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-16 19:30:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'dist', // ✅ This is required for Vercel to work smoothly
+  },
 });
